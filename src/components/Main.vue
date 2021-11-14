0605de465163f5ccc9bc25d5cfc5c8fd94472af4
Holger Mueller <9jholger@gmail.com>
2021-11-14 16:16:12 -0600

    removed unnecessary code

---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-card class="main-card">
+      <v-card-title> Text to speech </v-card-title>
+
+      <v-card-text>
+        <v-textarea
+          placeholder="Type your message here..."
+          outlined
+        ></v-textarea>
+
+        <h4>toxicity rating will appear here...</h4>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn block> Block </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainCard",
+};
+</script>
+
+<style scoped>
+.main-card {
+  width: 75%;
+  margin: 7% auto;
+}
+</style>
