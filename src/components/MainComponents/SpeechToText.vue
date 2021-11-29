1f670fbcdeac9cf6ad5149f813d44b42b39de929
Holger Mueller <9jholger@gmail.com>
2021-11-28 19:30:51 -0600

    got icon and message to toggle

--- src/components/MainComponents/SpeechToText.vue
+++ src/components/MainComponents/SpeechToText.vue
@@ -4,9 +4,30 @@
       <v-textarea outlined clearable></v-textarea>
     </v-card-text>
     <v-card-actions>
-      <v-btn elevation="0" dark large fab class="talk-button ma"
-        ><v-icon> mdi-microphone</v-icon></v-btn
-      >
+      <v-list class="talk-controls" dense>
+        <v-list-item>
+          <v-list-item-content>
+            <v-btn
+              id="recordButton"
+              elevation="0"
+              dark
+              large
+              fab
+              class="talk-button"
+              @click.prevent="toggleRecording"
+              ><v-icon> {{ recordingIcon }}</v-icon></v-btn
+            >
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-content>
+            <p id="recordingStatus">
+              {{ recordingMessage }}
+            </p>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-card-actions>
   </div>
 </template>
@@ -19,24 +40,65 @@ export default {
     runTimeTranscript: "",
     transcript: [],
     lang_: "es-ES",
+    recording: false,
+    controlDisplayData: [
+      {
+        icon: "mdi-microphone",
+        message: "Recording",
+      },
+      {
+        icon: "mdi-microphone-off",
+        message: "Not Recording",
+      },
+    ],
   }),
+
+  computed: {
+    recordingIcon() {
+      if (this.recording) {
+        return "mdi-microphone";
+      } else {
+        return "mdi-microphone-off";
+      }
+    },
+
+    recordingMessage() {
+      if (this.recording) {
+        return "Recording";
+      } else {
+        return "Not Recording";
+      }
+    },
+  },
 
   methods: {
     checkCompat() {
-      window.SpeechRecognitionAlternative =
-        window.SpeechRecognitionAlternative || window.webkitSpee;
+      // window.SpeechRecognition =
+      //   window.SpeechRecognition || window.webkitSpeechRecognition;
+      // const recognition = new SpeechRecognition();
+      // recognition.interimResults = true;
+      // if (!recognition) {
+      //   this.error =
+      //     "Speech recognition not availible on this browser.Please use Chrome or Firefox.";
+      // }
+      // recognition.addEventListener("result", (e) => {
+      //   const transcript = Array.from(e.results)
+      //     .map((result) => result[0])
+      //     .map((result) => result.transcript)
+      //     .join("");
+      //   console.log(transcript);
+      //});
+    },
 
-      if (!recognition) {
-        this.error =
-          "Speech recognition not availible on this browser.Please use Chrome or Firefox.";
-      }
+    toggleRecording() {
+      this.recording = !this.recording;
     },
   },
 };
 </script>
 
 <style scoped>
-.talk-button {
+.talk-controls {
   margin: auto;
 }
 </style>
