1eb26347d54eb3aa5b179d925e5e913b9bf525c9
Holger Mueller <9jholger@gmail.com>
2021-12-11 14:29:31 -0600

    fixed error in server

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const EXPRESS = require("express");
 const PATH = require("path");
+const MORGAN = require("morgan");
 
 const APP = EXPRESS();
+const PORT = process.env.PORT || 8081;
+
+APP.use(MORGAN("dev"));
 
 APP.get("*", (req, res) => {
   res.sendFile(PATH.resolve(__dirname, "./client/build", "index.html"));
 });
 
-APP.listen((PORT) => {
+APP.listen(PORT, () => {
   console.log(`==> API server now listening on PORT ${PORT}`);
 });
